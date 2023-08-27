763d7dee462c10679c7962871324927967df0066
lucas-zanetti <lucas.hzanetti@gmail.com>
2023-08-27 19:03:44 -0300

    Adding DTOs and setting automapping for them

---
+++ CleanArchMVC.Application/DTOs/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMVC.Application.DTOs
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Category Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string? Name { get; set; }
+    }
+}
---
+++ CleanArchMVC.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,45 @@
+﻿using CleanArchMVC.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CleanArchMVC.Application.DTOs
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Product Description is required")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Product Price is required")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Product Stock is required")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string? Image { get; set; }
+
+        public Category? Category { get; set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+    }
+}
