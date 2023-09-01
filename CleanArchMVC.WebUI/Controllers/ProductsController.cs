313def3917365b86a7058b02cd7d45aeb12ae0bf
lucas-zanetti <lucas.hzanetti@gmail.com>
2023-09-01 01:14:48 -0300

    Adding controller methods and view to delete, edit, create and show details of products

--- CleanArchMVC.WebUI/Controllers/ProductsController.cs
+++ CleanArchMVC.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,10 @@
-﻿using CleanArchMVC.Application.Interfaces;
+﻿using CleanArchMVC.Application.DTOs;
+using CleanArchMVC.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CleanArchMVC.WebUI.Controllers
@@ -7,10 +12,14 @@ namespace CleanArchMVC.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _productService = productService;
+            _categoryService = categoryService;
+            _environment = environment;
         }
 
         [HttpGet]
@@ -19,6 +28,103 @@ namespace CleanArchMVC.WebUI.Controllers
             var products = await _productService.GetProductsAsync();
             
             return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO product)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.AddProductAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var productDTO = await _productService.GetProductByIdAsync(id);
+
+            if (productDTO == null)
+                return NotFound();
+
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name", productDTO.CategoryId);
+
+            return View(productDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO product)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _productService.UpdateProductAsync(product);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var productDTO = await _productService.GetProductByIdAsync(id);
+
+            if (productDTO == null)
+                return NotFound();
+
+            return View(productDTO);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            await _productService.RemoveProductAsync(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var productDTO = await _productService.GetProductByIdAsync(id.Value);
+
+            if (productDTO == null)
+                return NotFound();
+
+            var wwwRoot = _environment.WebRootPath;
+            var image = Path.Combine(wwwRoot, "images\\" + productDTO.Image);
+            var existsImage = System.IO.File.Exists(image);
+
+            ViewBag.ImageExist = existsImage;
+
+            return View(productDTO);
         }
     }
 }
