ccd5f54674489e91fd462315378ccb969dca1242
lucas-zanetti <lucas.hzanetti@gmail.com>
2023-09-01 15:16:47 -0300

    Adding IAuthenticate and ISeedUserRoles interfaces to Domain

---
+++ CleanArchMVC.Domain/Account/IAuthenticate.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Account
+{
+    public interface IAuthenticate
+    {
+        Task<bool> AuthenticateAsync(string email, string password);
+        Task<bool> RegisterUserAsync(string email, string password);
+        Task LogoutAsync();
+    }
+}
