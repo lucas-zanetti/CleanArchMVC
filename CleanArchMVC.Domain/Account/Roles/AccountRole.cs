a38c7573818edf1ac0b58b5d6ed6a415981c1979
lucas-zanetti <lucas.hzanetti@gmail.com>
2023-09-01 15:47:47 -0300

    Adding Authanticate and SeedUserRole services and Db migration

---
+++ CleanArchMVC.Domain/Account/Roles/AccountRole.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchMVC.Domain.Account.Roles
+{
+    public enum AccountRole
+    {
+        Admin = 1,
+        User = 2
+    }
+}
---
+++ CleanArchMVC.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -0,0 +1,73 @@
+﻿using CleanArchMVC.Domain.Account;
+using CleanArchMVC.Domain.Account.Roles;
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace CleanArchMVC.Infra.Data.Identity
+{
+    public class SeedUserRoleInitial : ISeedUserRoleInitial
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedRoles()
+        {
+            SeedRole(AccountRole.User);
+            SeedRole(AccountRole.Admin);
+        }
+
+        public void SeedUsers()
+        {
+            SeedUser("usuario@localhost", "Senh@123", AccountRole.User);
+            SeedUser("admin@localhost", "Senh@123", AccountRole.Admin);
+        }
+
+        private void SeedUser(string email, string password, AccountRole accountRole)
+        {
+            string roleName = accountRole.ToString();
+
+            if (_userManager.FindByEmailAsync(email).Result == null)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = email,
+                    Email = email,
+                    NormalizedUserName = email.ToUpper(),
+                    NormalizedEmail = email.ToUpper(),
+                    EmailConfirmed = true,
+                    LockoutEnabled = false,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                };
+
+                IdentityResult result = _userManager.CreateAsync(user, password).Result;
+
+                if (result.Succeeded)
+                {
+                    _userManager.AddToRoleAsync(user, roleName).Wait();
+                }
+            }
+        }
+
+        private void SeedRole(AccountRole accountRole)
+        {
+            string roleName = accountRole.ToString();
+
+            if (!_roleManager.RoleExistsAsync(roleName).Result)
+            {
+                var role = new IdentityRole
+                {
+                    Name = roleName,
+                    NormalizedName = roleName.ToUpper(),
+                };
+
+                _roleManager.CreateAsync(role).Wait();
+            }
+        }
+    }
+}
